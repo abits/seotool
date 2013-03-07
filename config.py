2609da0abcc3ced6683d2711843561cd1bdbd09f
Chris Martel <chris@codeways.org>
2013-03-07 22:35:05 +0100

    Set secret key.

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = 'DrZy8)3*h#c(-k$a;Sy6YY<ÜT,=i$tLk]9üU_Rh>xb[fNJDcCjNfHucF.]Ä,yfQ,'
